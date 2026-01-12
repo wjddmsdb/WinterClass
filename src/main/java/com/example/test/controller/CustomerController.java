e8a82f209efa2006641b28077f2f537ad587677d
wjddmsdb <eunyuj648@gmail.com>
2026-01-12 14:09:47 +0900

    Commit to dev

--- src/main/java/com/example/test/controller/CustomerController.java
+++ src/main/java/com/example/test/controller/CustomerController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class CustomerController {
 //주석
     private final CustomerService customerService;
-
+//dev commit
     @GetMapping("/customers")
     public String customers(Model model) {
         model.addAttribute("customers", customerService.findAll());
